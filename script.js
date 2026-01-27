7eb235e8877f6d9d03d6f096e604ae4a45bdc1bb
ddiaaaaanna <dpuraite@yahoo.com>
2026-01-27 08:14:11 +0200

    finished tracking order card, started working on grooming card

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const orderTrackBtn = document.getElementById("header-order-tracking-btn");
 
+const orderTrackBox = document.querySelector(".order-track-box-hidden");
+
 orderTrackBtn.addEventListener("click", () => {
-  const orderTrackBox = document.querySelector(".order-track-box-hidden");
   orderTrackBox.classList.remove("order-track-box-hidden");
   orderTrackBox.classList.add("order-track-box-active");
 });
 
-console.log(orderTrackBox);
+const orderTrackExitBnt = document.getElementById("exit-small-btn");
+
+orderTrackExitBnt.addEventListener("click", () => {
+  orderTrackBox.classList.remove("order-track-box-active");
+  orderTrackBox.classList.add("order-track-box-hidden");
+});
