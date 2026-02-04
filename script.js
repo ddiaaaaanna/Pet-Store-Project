6e0ddbe407d0a3fa84f2d9b2c24f71dad499f8d4
ddiaaaaanna <dpuraite@yahoo.com>
2026-02-04 08:25:00 +0200

    work on searchbar

--- script.js
+++ script.js
@@ -2,26 +2,22 @@ const orderTrackBtn = document.getElementById("header-order-tracking-btn");
 const orderTrackBox = document.getElementById("order-track-box");
 
 orderTrackBtn.addEventListener("click", () => {
-  orderTrackBox.classList.remove("popup-box-hidden");
-  orderTrackBox.classList.add("popup-box-active");
+  orderTrackBox.classList.remove("hidden");
 });
 
 const groomingBtn = document.getElementById("header-grooming-btn");
 const groomingBox = document.getElementById("grooming-box");
 
 groomingBtn.addEventListener("click", () => {
-  groomingBox.classList.remove("popup-box-hidden");
-  groomingBox.classList.add("popup-box-active");
+  groomingBox.classList.remove("hidden");
 });
 
-const orderTrackExitBtns = document.querySelectorAll(".exit-small-btn");
+const popupCloseButtons = document.querySelectorAll(".popup-close-btn");
 
-orderTrackExitBtns.forEach((btn) => {
+popupCloseButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    orderTrackBox.classList.remove("popup-box-active");
-    orderTrackBox.classList.add("popup-box-hidden");
-    groomingBox.classList.remove("popup-box-active");
-    groomingBox.classList.add("popup-box-hidden");
+    orderTrackBox.classList.add("hidden");
+    groomingBox.classList.add("hidden");
   });
 });
 
@@ -29,11 +25,13 @@ const searchInput = document.getElementById("searchbar");
 const searchDropdown = document.getElementById("search-bar-dropdown");
 
 searchInput.addEventListener("focus", () => {
-  searchDropdown.classList.remove("search-bar-dropdown-hidden");
-  searchDropdown.classList.add("search-bar-dropdown");
+  searchDropdown.classList.remove("hidden");
 });
 
-searchInput.addEventListener("blur", () => {
-  searchDropdown.classList.remove("search-bar-dropdown");
-  searchDropdown.classList.add("search-bar-dropdown-hidden");
+const searchBarNav = document.getElementById("search-bar-nav");
+
+document.addEventListener("click", (event) => {
+  if (!searchBarNav.contains(event.target)) {
+    searchDropdown.classList.add("hidden");
+  }
 });
