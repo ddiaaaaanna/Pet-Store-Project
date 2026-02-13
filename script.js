4b6788d91145b30dd686d2c2e02e8268f9f6cbd0
ddiaaaaanna <dpuraite@yahoo.com>
2026-02-13 07:59:27 +0200

    WIP: add dog deals page, remove modals, carousel setup

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const orderTrackBtn = document.getElementById("header-order-tracking-btn");
-const orderTrackBox = document.getElementById("order-track-box");
-
-orderTrackBtn.addEventListener("click", () => {
-  orderTrackBox.classList.remove("hidden");
-});
-
-const groomingBtn = document.getElementById("header-grooming-btn");
-const groomingBox = document.getElementById("grooming-box");
-
-groomingBtn.addEventListener("click", () => {
-  groomingBox.classList.remove("hidden");
-});
-
-const popupCloseButtons = document.querySelectorAll(".popup-close-btn");
-
-popupCloseButtons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    orderTrackBox.classList.add("hidden");
-    groomingBox.classList.add("hidden");
-  });
-});
-
 const searchInput = document.getElementById("searchbar");
 const searchDropdown = document.getElementById("search-bar-dropdown");
 
@@ -35,3 +12,7 @@ document.addEventListener("click", (event) => {
     searchDropdown.classList.add("hidden");
   }
 });
+
+const carrouselNextBtn = document.getElementById("carousel-next");
+
+carrouselNextBtn.addEventListener("click", () => {});
